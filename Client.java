b077c22580ff8556314d0e350a82ec27a6792515
Chirag <chirag.dev18@gmail.com>
2023-04-27 17:44:31 +0530

    GUI Added

--- Client.java
+++ Client.java
@@ -1,76 +1,151 @@
 import java.io.*;
 import java.net.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-class Client {
+class Client extends JFrame {
   Socket socket;
   BufferedReader br;
-  BufferedReader br_server;
+  PrintWriter out;
 
   DataOutputStream dout;
   DataInputStream din_server;
 
+  // GUI
+  public JLabel heading = new JLabel("Client Area");
+  public JTextArea messageArea = new JTextArea();
+  // public JTextArea messageAreaUser = new JTextArea();
+
+  public JTextField messageInput = new JTextField();
+  public Font font = new Font("Roboto", Font.PLAIN, 20);
+
   public Client() throws IOException {
-    System.out.println("Client Started ");
     socket = new Socket("localhost", 3333);
 
-    sendData();
-    getData();
+    br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    out = new PrintWriter(socket.getOutputStream());
+    try {
+      gui();
+      handleEvents();
+      getData();
+      // sendData();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void gui() {
+    this.setTitle("Client Messager");
+    this.setSize(300, 400);
+
+    // this.setLocationRelativeTo(null);
+    this.setLocation(new Point(400,200));
+
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    // Components
+    heading.setFont(font);
+
+    messageArea.setFont(font);
+    messageInput.setFont(font);
+
+    heading.setHorizontalAlignment(SwingConstants.CENTER);
+    heading.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+    messageArea.setEditable(false);
+    this.setLayout(new BorderLayout());
+
+    // Adding comp
+    this.add(heading, BorderLayout.NORTH);
+    JScrollPane jScrollPane = new JScrollPane(messageArea);
+    
+    this.add(jScrollPane, BorderLayout.CENTER);
+    this.add(messageInput, BorderLayout.SOUTH);
+
+    this.setVisible(true);
+
+  }
+
+  void handleEvents() {
+    messageInput.addKeyListener(new KeyListener() {
+      @Override
+      public void keyTyped(KeyEvent e) {
+
+        // System.out.println("Key Typed");
+      }
+
+      @Override
+      public void keyPressed(KeyEvent e) {
+        // System.out.println("Key Pressed");
+
+      }
+
+      @Override
+      public void keyReleased(KeyEvent e) {
+        // System.out.println("Key Released");
+        if (e.getKeyCode() == 10) {
+          String msgToSend = messageInput.getText();
+          messageArea.append("Me : " + msgToSend + "\n");
+          out.println(msgToSend);
+          out.flush();
+
+          messageInput.setText("");
+          messageInput.requestFocus();
+        }
+      }
+
+    });
   }
 
   void sendData() {
+
     System.out.println("In sendData...");
 
     Runnable r1 = () -> {
       try {
-        dout = new DataOutputStream(socket.getOutputStream());
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String str = "", str2 = "";
         while (!socket.isClosed()) {
-          if (str.equals("stop")) {
-            System.out.println("Client Stopped :");
-            return;
+          String content = br.readLine();
+          out.println(content);
+          out.flush();
+          if (content.equals("exit")) {
+            socket.close();
+            break;
           }
-
-          // For Sending from server
-          str2 = br.readLine();
-          dout.writeUTF(str2);
-
-          dout.flush();
         }
-      } catch (Exception e) {
-        e.printStackTrace();
+      } catch (Exception err) {
+        System.out.println("connection closed");
       }
-      System.out.println("Sending Data");
     };
 
     new Thread(r1).start();
   }
 
   void getData() {
-    Runnable r2 = () -> {
-      System.out.println("In GetData...");
+    Runnable r2 =() ->{
+      try{
 
-      try {
-        din_server = new DataInputStream(socket.getInputStream());
-        String str = din_server.readUTF();
+        while(true){
 
-        while (!socket.isClosed()) {
-          if (str.equals("stop")) {
+          String content = br.readLine();
+          
+          if (content.equals("exit")) {
             System.out.println("Stopped..");
-            return;
+            JOptionPane.showMessageDialog(this,"Server Ended");
+            socket.close();
+            break;
           }
-          str = din_server.readUTF();
-          System.out.println("Server says: " + str);
+          messageArea.append("Server : " + content + "\n");
+
         }
-      } catch (Exception e) {
-        e.printStackTrace();
+      }catch(Exception err){
+        System.out.println("Err");
       }
     };
     new Thread(r2).start();
   }
-
+    
   public static void main(String args[]) throws IOException {
     new Client();
   }
--- Server.java
+++ Server.java
@@ -1,50 +1,118 @@
 import java.io.*;
 import java.net.*;
+// Gui
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-class Server {
+class Server extends JFrame{
   ServerSocket server;
   Socket socket;
-  BufferedReader br_server;
 
-  DataOutputStream dout;
+  BufferedReader br;
+  PrintWriter out;
 
-  DataInputStream din_server;
+  // GUI
+  public JLabel heading = new JLabel("Server Area");
+  public JTextArea messageArea = new JTextArea();
+  public JTextField messageInput = new JTextField();
+  public Font font = new Font("Roboto", Font.PLAIN, 20);
 
   public Server() throws IOException {
-    System.out.println("Server Started :");
 
     server = new ServerSocket(3333);
     socket = server.accept();
-    sendData();
-    getData();
+
+    br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+    out = new PrintWriter(socket.getOutputStream());
+
+    try{
+      gui();
+      handleEvents();
+      // sendData();
+      getData();
+    }
+    catch(Exception err){
+      System.out.println("Error");
+    }
+  }
+
+  
+  public void gui() {
+    this.setTitle("Server Messager");
+    this.setSize(300, 400);
+
+    this.setLocation(new Point(100,200));
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    // Components
+    heading.setFont(font);
+
+    messageArea.setFont(font);
+    messageInput.setFont(font);
+
+    heading.setHorizontalAlignment(SwingConstants.CENTER);
+    heading.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+    messageArea.setEditable(false);
+    this.setLayout(new BorderLayout());
+
+    // Adding comp
+    this.add(heading, BorderLayout.NORTH);
+    JScrollPane jScrollPane = new JScrollPane(messageArea);
+    this.add(jScrollPane, BorderLayout.CENTER);
+    this.add(messageInput, BorderLayout.SOUTH);
+
+    this.setVisible(true);
+
+  }
+
+  void handleEvents() {
+    messageInput.addKeyListener(new KeyListener() {
+      @Override
+      public void keyTyped(KeyEvent e) {
+
+        // System.out.println("Key Typed");
+      }
+
+      @Override
+      public void keyPressed(KeyEvent e) {
+        // System.out.println("Key Pressed");
+
+      }
+
+      @Override
+      public void keyReleased(KeyEvent e) {
+        // System.out.println("Key Released");
+        if (e.getKeyCode() == 10) {
+          String msgToSend = messageInput.getText();
+          messageArea.append("Me : " + msgToSend + "\n");
+          out.println(msgToSend);
+          out.flush();
+
+          messageInput.setText("");
+          messageInput.requestFocus();
+        }
+      }
+
+    });
   }
 
   void sendData() {
-    System.out.println("In sendData...");
     
     Runnable r1 = () -> {
-      try {
-        dout = new DataOutputStream(socket.getOutputStream());
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String str = "", str2 = "";
-        while (!server.isClosed()) {
-          if (str.equals("stop")) {
-            System.out.println("Server Stopped :");
-            return;
-          }
-
-          // For Sending from server
-          str2 = br.readLine();
-          dout.writeUTF(str2);
-
-          dout.flush();
+      while (true) {
+        try {        
+        String content = messageInput.getText();
+        out.println(content);
+        out.flush();
+        } catch (Exception e) {
+          e.printStackTrace();
         }
-      } catch (Exception e) {
-        e.printStackTrace();
+        // System.out.println("Sending Data");
       }
-      System.out.println("Sending Data");
     };
 
     new Thread(r1).start();
@@ -52,19 +120,18 @@ class Server {
 
   void getData() {
     Runnable r2 = () -> {
-      System.out.println("In GetData...");
-
       try {
-        din_server = new DataInputStream(socket.getInputStream());
-        String str = din_server.readUTF();
+        while (true) {
+          String msg = br.readLine();
 
-        while (!server.isClosed()) {
-          if (str.equals("stop")) {
+          if (msg.equals("exit")) {
             System.out.println("Stopped..");
-            return;
+            JOptionPane.showMessageDialog(this,"Server Ended");
+            socket.close();
+            break;
           }
-          str = din_server.readUTF();
-          System.out.println("Client says: " + str);
+          // System.out.println("Client says: " + msg);
+          messageArea.append("Client : " + msg + "\n");
         }
       } catch (Exception e) {
         e.printStackTrace();
